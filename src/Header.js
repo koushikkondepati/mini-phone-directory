d842c68b567d9ff6d06c35596317f2a29409c722
koushik k <koushik@billionlives.in>
2021-09-18 16:52:26 +0530

    Displayed phone directory

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = function(){
+    return(
+        <div className="headerName">
+            phone directory
+        </div>
+    )
+};
+
+export default Header;
