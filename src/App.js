fc8807a270ddb5561483554e0ac511ebd309e7e9
koushik k <koushik@billionlives.in>
2021-09-20 00:00:41 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,10 +3,43 @@ import React, {Component} from 'react';
 import Header from "./Header";
 
 class App extends Component{
+
+  
+
   render(){
+    let subscribers = [
+      {
+        id: 1,
+        name: "koushik",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Ratan",
+        phone: "9999999999"
+      }
+    ]
     return(
       <div>
         <Header />
+        <div style={{margin:10}}>
+          <button className='addButton'>ADD</button>
+        
+          <div>
+            <span>Name</span>
+            <span>Phone</span>
+          </div>
+
+          {
+            subscribers.map( sub =>{
+              return <div key={sub.id}>
+              <span>{sub.name}</span>
+              <span>{sub.phone}</span>
+            </div>
+            })
+          }
+
+        </div>
       </div>
     );
   }
